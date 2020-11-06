df9c853e839d9d01b671bc827899b61943adf35b
ATCDev41 <alex.atc.digital@gmail.com>
2020-11-06 03:54:00 +0100

    add password to user + bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_002213) do
+ActiveRecord::Schema.define(version: 2020_11_06_024812) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_002213) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
