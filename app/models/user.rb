bb590d1f44404fbf87a84ef45d5bc6e887478d09
ATCDev41 <alex.atc.digital@gmail.com>
2020-11-06 02:07:10 +0100

    associations users/articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
+
+  has_many :articles
+
+  before_save { self.email.downcase! }
   validates :username, presence: true,
               uniqueness: { case_sensitive: false },
               length: {minimum: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /.+\@.+\..+/i
 
+
   validates :email, presence: true,
             uniqueness: { case_sensitive: false },
-            format: {with: VALID_EMAIL_REGEX }
+            format: {with: VALID_EMAIL_REGEX}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_230942) do
+ActiveRecord::Schema.define(version: 2020_11_06_002213) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
